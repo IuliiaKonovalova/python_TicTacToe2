35a61f392a479eea7ebd9d9154fd8900ec5d551b
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-07 18:34:56 +0800

    Add main function to start the game

--- run.py
+++ run.py
@@ -77,8 +77,12 @@ def play(game, x_player, o_player, print_game=True):
                     print(f'{letter} wins!')
 
             letter = 'O' if letter == 'X' else 'X'
-
+        
         if print_game:
             print("It's a tie!")
 
-if __name__ = 
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
