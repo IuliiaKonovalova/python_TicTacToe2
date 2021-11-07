086d00e77eda95f4e33652c4d338437d0386b0c4
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-07 17:51:25 +0800

    Add classes which inherit from player class

--- players.py
+++ players.py
@@ -9,3 +9,20 @@ class Player:
         pass
 
 
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square  = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9: '))
