6855bcf15803fefce518c60c2342cefd2d40bf16
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-07 17:26:49 +0800

    Add class tictactoe

---
+++ run.py
@@ -0,0 +1,9 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print(f'''| {'|'.join(row)} |''')
+
