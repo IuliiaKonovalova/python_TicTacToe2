36baf23e6150fd502330ec7cefcc87df74eb17d0
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-07 17:56:23 +0800

    Add functionality to the method get move for the user

--- players.py
+++ players.py
@@ -25,4 +25,12 @@ class HumanPlayer(Player):
         valid_square  = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-9: '))
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
