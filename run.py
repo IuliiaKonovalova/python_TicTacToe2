ca737ddb5c17a982ba7deaf608280242623e3073
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-07 17:31:20 +0800

    Add method to the tictactoe class to print the board with positions

--- run.py
+++ run.py
@@ -5,5 +5,12 @@ class TicTacToe:
 
     def print_board(self):
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
-            print(f'''| {'|'.join(row)} |''')
+            print(f'''| {' | '.join(row)} |''')
 
+    @staticmethod
+    def print_board_num():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+          print(f'''| {' | '.join(row)} |''')
+
+print(TicTacToe.print_board_num())
